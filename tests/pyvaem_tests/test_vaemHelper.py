d91ad359011bc3a1eb4a673a35b02f0b756e84bf
Jose <jlmoraleshellin@gmail.com>
2025-05-27 16:11:35 +0200

    test: add unit tests for VaemRegisters creation from list

--- tests/pyvaem_tests/test_vaemHelper.py
+++ tests/pyvaem_tests/test_vaemHelper.py
@@ -1,3 +1,19 @@
-def test_whatever():
-    assert False
+from pyvaem.vaemHelper import VaemRegisters
+
+def test_when_registers_created_from_empty_list_returns_none():
+    registers = VaemRegisters.from_list([])
+    assert registers is None
+
+def test_when_registers_created_from_list_returns_as_expected():
+    registers = VaemRegisters.from_list([1, 2, 3, 4, 5, 6])
+    assert registers == VaemRegisters(
+        access=0,
+        dataType=1,
+        paramIndex=2,
+        paramSubIndex=0,
+        errorRet=3,
+        transferValue=0x3000400050006
+    )
+
+
 
