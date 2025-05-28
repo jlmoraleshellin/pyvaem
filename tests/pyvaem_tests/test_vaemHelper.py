a6d556f03b1f751d66436005d4aad681643c8ec6
Jose <jlmoraleshellin@gmail.com>
2025-05-28 13:12:01 +0200

    test: add parameterized test for VaemRegisters to_list method

--- tests/pyvaem_tests/test_vaemHelper.py
+++ tests/pyvaem_tests/test_vaemHelper.py
@@ -30,6 +30,29 @@ def test_when_registers_created_from_list_returns_as_expected(
     assert registers == expected_registers
 
 
+@pytest.mark.parametrize(
+    "registers, expected_list",
+    [
+        (
+            VaemRegisters(
+                access=0,
+                dataType=1,
+                paramIndex=2,
+                paramSubIndex=0,
+                errorRet=3,
+                transferValue=0x3000400050006,
+            ),
+            [1, 2, 3, 3, 4, 5, 6],
+        ),
+    ],
+)
+def test_settings_returned_as_list_returns_are_as_expected(
+    registers: "VaemRegisters", expected_list
+):
+    register_list = registers.to_list()
+    assert register_list == expected_list
+
+
 def test_settings_returned_as_enum_dict_are_as_expected():
     settings = ValveSettings()
     assert settings.to_enum_dict() == {
