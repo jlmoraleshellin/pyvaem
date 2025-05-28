e6906a56fd58f353f36e9bae4fa514d2447957b9
Jose <jlmoraleshellin@gmail.com>
2025-05-28 09:32:04 +0200

    refactor: enhance tests for VaemRegisters and add ValveSettings validation

--- tests/pyvaem_tests/test_vaemHelper.py
+++ tests/pyvaem_tests/test_vaemHelper.py
@@ -1,19 +1,43 @@
-from pyvaem.vaemHelper import VaemRegisters
+import pytest
+from pyvaem.vaemHelper import VaemIndex, VaemRegisters, ValveSettings
+
 
 def test_when_registers_created_from_empty_list_returns_none():
     registers = VaemRegisters.from_list([])
     assert registers is None
 
-def test_when_registers_created_from_list_returns_as_expected():
-    registers = VaemRegisters.from_list([1, 2, 3, 4, 5, 6])
-    assert registers == VaemRegisters(
-        access=0,
-        dataType=1,
-        paramIndex=2,
-        paramSubIndex=0,
-        errorRet=3,
-        transferValue=0x3000400050006
-    )
+
+@pytest.mark.parametrize(
+    "input_list, expected_registers",
+    [
+        (
+            [1, 2, 3, 4, 5, 6],
+            VaemRegisters(
+                access=0,
+                dataType=1,
+                paramIndex=2,
+                paramSubIndex=0,
+                errorRet=3,
+                transferValue=0x3000400050006,
+            ),
+        ),
+    ],
+)
+def test_when_registers_created_from_list_returns_as_expected(
+    input_list, expected_registers
+):
+    registers = VaemRegisters.from_list(input_list)
+    assert registers == expected_registers
 
 
-
+def test_settings_returned_as_enum_dict_are_as_expected():
+    settings = ValveSettings()
+    assert settings.to_enum_dict() == {
+        VaemIndex.NominalVoltage: 24000,
+        VaemIndex.ResponseTime: 500,
+        VaemIndex.TimeDelay: 0,
+        VaemIndex.PickUpTime: 125,
+        VaemIndex.InrushCurrent: 300,
+        VaemIndex.HitNHold: 100,
+        VaemIndex.HoldingCurrent: 100,
+    }
